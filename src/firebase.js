195c5b52f25d6b6da2fd62d24a369ef89ad82e01
MIN <65863834+keimindev@users.noreply.github.com>
2021-12-04 20:43:37 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrpvweTRudFF_x6T2vEw4_m_tCPNJsbFM",
-  authDomain: "mymagazine-374c0.firebaseapp.com",
-  projectId: "mymagazine-374c0",
-  storageBucket: "mymagazine-374c0.appspot.com",
-  messagingSenderId: "827951285550",
-  appId: "1:827951285550:web:2c26390484d2b01a16e6bf"
+ 
 };
 
 // Initialize Firebase
